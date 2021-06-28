8f878c33222773aa41337bb3d2e0a4d8958fb76a
Duane Miles <29195796+TheDeadlyPianist@users.noreply.github.com>
2021-06-28 13:43:53 +0100

    [DM] [documentation-setup] Created base documentation for the URL helper

---
+++ siteFiles/main.js
@@ -0,0 +1,16 @@
+function getComponentContent(contentLink) {
+    var xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200) {
+            var element = document.getElementById("page-content")
+            element.hidden = false;
+            element.innerHTML = xhttp.responseText;
+        }
+    }
+    xhttp.open("GET", contentLink, true);
+    xhttp.send();
+}
+
+document.getElementById("auth-login-url").addEventListener("click", function () {
+    getComponentContent("siteFiles/components/authLoginUrlComponent.html");
+})
